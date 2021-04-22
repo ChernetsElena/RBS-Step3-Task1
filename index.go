4d349c20112058e1ec7167d1d40831207a4fab8c
ChernetsElena <chernets-elena@mail.ru>
2021-04-22 16:17:40 +0200

    pravki

--- index.go
+++ index.go
@@ -17,38 +17,44 @@ func main() {
 		dir      *string
 	)
 
+	// создание файла для логов
 	logFile, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer logFile.Close()
 
+	// создание логов
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	infoLogFile := log.New(logFile, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLogFile := log.New(logFile, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	datafile = flag.String("datafile", "urls.txt", `Path to datafile. Default: "urls.txt"`)
+	datafile = flag.String("datafile", "urls.txt", `Path to datafile."`)
 	dir = flag.String("dir", "./dir/", `Path to dir. Default: "./dir/"`)
 
 	flag.Parse()
 
 	// открытие файла
-	f, err := os.Open(*datafile)
+	urlFile, err := os.Open(*datafile)
 	if err != nil {
 		errorLog.Println(err)
 		errorLogFile.Println(err)
 		os.Exit(1)
-		return
 	}
-	defer f.Close()
+	defer urlFile.Close()
 	infoLog.Printf("Открытие файла: %s\n", *dir+*datafile)
 	infoLogFile.Printf("Открытие файла: %s\n", *dir+*datafile)
+
+	// создание директории
+	if _, err := os.Stat(*dir); os.IsNotExist(err) {
+		os.MkdirAll(*dir, 0777)
+	}
 
 	// чтение файла
 	infoLog.Printf("Чтение файла: %s\n", *dir+*datafile)
 	infoLogFile.Printf("Чтение файла: %s\n", *dir+*datafile)
-	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(urlFile)
 
 	for scanner.Scan() {
 		address := string(scanner.Text())
@@ -58,10 +64,8 @@ func main() {
 		fileName = strings.Replace(fileName, "http://", "", -1)
 		fileName = strings.Replace(fileName, "/", ".", -1)
 
-		// создание файла и директории
-		if _, err := os.Stat(*dir); os.IsNotExist(err) {
-			os.MkdirAll(*dir, 0777)
-		}
+		// создание файла
+		//pathjoin добавить
 		file, err := os.Create(*dir + fileName + ".html")
 		if err != nil {
 			errorLog.Println("Unable to create file:", err)
@@ -85,7 +89,14 @@ func main() {
 	}
 }
 
-func MakeRequest(address string, errorLog *log.Logger, infoLog *log.Logger, errorLogFile *log.Logger, infoLogFile *log.Logger) (body []byte) {
+// функция отправляет запрос и получает данные
+func MakeRequest(
+	address string,
+	errorLog *log.Logger,
+	infoLog *log.Logger,
+	errorLogFile *log.Logger,
+	infoLogFile *log.Logger) (body []byte) {
+
 	resp, err := http.Get(address)
 	if err != nil {
 		errorLog.Println(err)
@@ -106,3 +117,10 @@ func MakeRequest(address string, errorLog *log.Logger, infoLog *log.Logger, erro
 
 	return
 }
+
+//нужно ли закрывать resp
+//сделать функции info, error будут выводить ошибку и инфо в консоль и файл, либо только в консоль , в зависимости от установленного флага
+//при запуске программы
+//defer scanner как работают
+//обработать ошибку, когда один из юрл может быть не корректным, а остальные корректны, программа не должна вылетать
+// в функции makeRequest также сделать чтобы не вылетала, а просто возвращала ошибку
